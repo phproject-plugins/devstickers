5c5dc54dfce2a98ec985aac410df7879c893546e
Alan Hardman <ahardman@thrivelife.com>
2015-06-04 11:45:04 -0600

    Adding title attribute to images

--- base.php
+++ base.php
@@ -14,9 +14,9 @@ class Base extends \Plugin {
 	 */
 	public function _load() {
 		$this->_hook("text.parse.after", function($str) {
-			$base = \Base::instance()->get("BASE");
-			return preg_replace_callback("/:(404|btb|cannot-reproduce|codepush|compiling|facepalm|hack|helloworld|hotfix-denied|hotfix|idk|itrl|iwy|lgtm|loading|null|outofscope|plzwork|review|shipit|wfm|woot):/", function($matches) use($base) {
-				return "<img style=\"width: 90px; height: 90px;\" src=\"{$base}/app/plugin/devstickers/img/{$matches[1]}.png\" alt=\"{$matches[0]}\">";
+			return preg_replace_callback("/:(404|btb|cannot-reproduce|codepush|compiling|facepalm|hack|helloworld|hotfix-denied|hotfix|idk|itrl|iwy|lgtm|loading|null|outofscope|plzwork|review|shipit|wfm|woot):/", function($matches) {
+				$base = \Base::instance()->get("BASE");
+				return "<img style=\"width: 90px; height: 90px;\" src=\"{$base}/app/plugin/devstickers/img/{$matches[1]}.png\" title=\"{$matches[0]}\" alt=\"{$matches[0]}\">";
 			}, $str);
 		});
 	}
