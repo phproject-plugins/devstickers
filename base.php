33290cad4552366fc718236c52533365bdfb29b3
Alan Hardman <alanaktion@gmail.com>
2025-07-09 22:41:32 +0000

    PHP 8 + PSR-12

--- base.php
+++ base.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package  DevStickers
  * @author   Alan Hardman <alan@phpizza.com>
@@ -7,18 +8,16 @@
 
 namespace Plugin\DevStickers;
 
-class Base extends \Plugin {
-
-	/**
-	 * Initialize the plugin
-	 */
-	public function _load() {
-		$this->_hook("text.parse.after", function($str) {
-			return preg_replace_callback("/:(404|btb|cannot-reproduce|codepush|compiling|facepalm|hack|helloworld|hotfix-denied|hotfix|idk|itrl|iwy|lgtm|loading|null|outofscope|plzwork|review|shipit|wfm|woot):/", function($matches) {
-				$base = \Base::instance()->get("BASE");
-				return "<img style=\"width: 90px; height: 90px;\" src=\"{$base}/app/plugin/devstickers/img/{$matches[1]}.png\" title=\"{$matches[0]}\" alt=\"{$matches[0]}\">";
-			}, $str);
-		});
-	}
-
+class Base extends \Plugin
+{
+    /**
+     * Initialize the plugin
+     */
+    public function _load()
+    {
+        $this->_hook("text.parse.after", fn(string $str): ?string => preg_replace_callback("/:(404|btb|cannot-reproduce|codepush|compiling|facepalm|hack|helloworld|hotfix-denied|hotfix|idk|itrl|iwy|lgtm|loading|null|outofscope|plzwork|review|shipit|wfm|woot):/", function (array $matches): string {
+            $base = \Base::instance()->get("BASE");
+            return "<img style=\"width: 90px; height: 90px;\" src=\"{$base}/app/plugin/devstickers/img/{$matches[1]}.png\" title=\"{$matches[0]}\" alt=\"{$matches[0]}\">";
+        }, $str));
+    }
 }
